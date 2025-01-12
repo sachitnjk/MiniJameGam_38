8ca19999cbace4476f7ca3a7e60f291e7e500267
sachitnjk <sachitnjk@gmail.com>
2025-01-12 18:34:36 +0530

    Added appearance to side thief view

--- Assets/Scripts/ThiefInfoPanelAssigner.cs
+++ Assets/Scripts/ThiefInfoPanelAssigner.cs
@@ -8,38 +8,13 @@ public class ThiefInfoPanelAssigner : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI nameText;
     [SerializeField] private TextMeshProUGUI tierText;
-    [SerializeField] private Image headAppearance;
-    [SerializeField] private Image bodyAppearance;
-    
-    [SerializeField] private Sprite gruntBody;
-    [SerializeField] private Sprite MinionBody;
-    [SerializeField] private Sprite BruteBody;
-    [SerializeField] private Sprite ProfessionalBody;
+    [SerializeField] private ThiefAppearanceHandler appearanceHandler;
 
     public void SetupUI(ThiefData thiefData)
     {
-        headAppearance.sprite = thiefData.ThiefHeadAppearance;
+        appearanceHandler.SetHeadSprite(thiefData.ThiefHeadAppearance);
         nameText.text = thiefData.Name;
         tierText.text = thiefData.Tier.ToString();
-        SetTier(thiefData.Tier);
-    }
-    
-    public void SetTier(ThiefTiers tier)
-    {
-        switch (tier)
-        {
-            case ThiefTiers.Grunt:
-                bodyAppearance.sprite = gruntBody;
-                break;
-            case ThiefTiers.Minion:
-                bodyAppearance.sprite = MinionBody;
-                break;
-            case ThiefTiers.Brute:
-                bodyAppearance.sprite = BruteBody;
-                break;
-            case ThiefTiers.Professional:
-                bodyAppearance.sprite = ProfessionalBody;
-                break;
-        }
+        appearanceHandler.SetTier(thiefData.Tier);
     }
 }
